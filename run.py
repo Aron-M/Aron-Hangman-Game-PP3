7923b1dec2eb94e06e9456082317476a85e52529
Aron-M <aron.moelich88@gmail.com>
2023-01-13 14:33:14 +0000

    copied initial python code from repository named https://aronm-hangmangamepp3py-fzwo7axw0bi.ws-eu82.gitpod.io/ into run.py

--- run.py
+++ run.py
@@ -1,105 +1,99 @@
 import random
-
-
 def hangman():
-    hangman = [
+	hangman1 = [
+	"""
++----+
+     |
+     |
+     |
+     |
+     |
+     ===""", """
++----+
+  |  |
+  O  |
+     |
+     |
+     |
+     ===""", """
++----+
+  |  | 
+  O  |
+ /   |
+     |
+     |
+     ===""", """
++----+
+  |  |
+  O  |
+ /|  |
+     |
+     |
+     ===""", """
++----+
+  |  |
+  O  |
+ /|\ |
+     |
+     |
+     ===""", """
+
++----+
+  |  |
+  O  |
+ /|\ |
+  |  |
+     | 
+     ===""", """
++----+
+  |  |
+  O  |
+ /|\ |
+  |  |
+ /   | 
+     ===""", """
++----+
+  |  |
+  O  |
+ /|\ |
+  |  | 
+ / \ | 
+     ===""", """
+     
 """
-+---+
-    |
-    |
-    |
-    |
-    |
-    ===""", """
-+---+
-  | |
-  O |
-    |
-    |
-    |
-    ===""", """
-+---+
-  | |
-  O |
- /  |
-    |
-    |
-    ===""", """
-+---+
-  | |
-  O |
- /| |
-    |
-    |
-    ===""", """
-+---+
-  | |
-  O |
- /|\|
-    |
-    | 
-    ===""", """
-
-+---+
-  | |
-  O |
- /|\|
-  | | 
-    | 
-    ===""", """
-+---+
-  | |
-  O |
- /|\|
-  | | 
- /  | 
-    ===""", """
-+---+
-  | |
-  O |
- /|\|
-  | | 
- / \| 
-    ===""", """
-"""
-]
-
-words = ['balloon', 'aeroplane', 'computer', 'corridor', 'television', 'university', 'paraglide', 'submarine', 'ferriswheel']
-
-word = random.choice(words).lower()
+	]
+	words = ['balloon', 'aeroplane', 'computer', 'corridor', 'television', 'university', 'paraglide', 'submarine', 'ferriswheel']
+	word = random.choice(words).lower()
 
 
-guessed_right = []
-guessed_wrong = []
-tries = 8
-hangman_count = -1
+	guessed_right = []
+	guessed_wrong = []
+	tries = 8
+	hangman_count = -1
 
-while tries > 0:
-    output = ''
-    for letter in word:
-        if letter in guessed_right:
-            output += letter
-        else:
-            output += '_'
-    if output == word:
-        break
-    print("Try to guess the correct word: ",output)
-    print("You have ", tries," chances left")
-    guess = input().lower()
-    if guess in guessed_right or guess in guessed_wrong:
-        print("You have already guessed", guess, " ,please try again!")
-    elif guess in word:
-        print("That was great! Well done, you guessed the right letter!")
-        guessed_right.append(guess)
-    else:
-        print("Sorry, but you have guessed a wrong letter! Please try again.")
-        hangman_count = hangman_count + 1
-        tries = tries-1
-        guessed_wrong.append(guess)
-        print(hangman1[hangman_count])
-    
-    if tries == 0:
-        print("GAME OVER, YOU LOST!")
-    
-    if __name__ == "__main__":
-        hangman()
+	while tries > 0:
+		output = ''
+		for letter in word:
+			if letter in guessed_right:
+				output += letter
+			else:
+				output += '_'
+		if output == word:
+			break
+		print("Try to guess the correct word: ",output)
+		print("You have ", tries," chances left")
+		guess = input().lower()
+		if guess in guessed_right or guess in guessed_wrong:
+			print("You have already guessed", guess, " ,please try again!")
+		elif guess in word:
+			print("That was great! Well done, you guessed the right letter!")
+			guessed_right.append(guess)
+		else:
+			print("Sorry, but you have guessed a wrong letter! Please try again.")
+			hangman_count = hangman_count + 1
+			tries = tries-1
+			guessed_wrong.append(guess)
+			print(hangman1[hangman_count])
+			
+if __name__ == "__main__":
+    hangman()

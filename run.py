162b08a70deaa526c4a25fd8b6f0fba9e3e97be7
Aron-M <aron.moelich88@gmail.com>
2023-01-14 10:58:52 +0000

    adding imports and SCOPE

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
 import random
 def hangman():
 	hangman1 = [
